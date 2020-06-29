32f81cab99d88a494d2651a8a980f093fff3a906
sudo-corvus <neo11prince@gmail.com>
2020-06-29 19:47:45 +0530

    server ready to kickoff

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const auth = require('./routes/api/auth');
+
+//App init
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+
+
+
+
+// Init Middleware
+app.use(express.json());
+
+
+//User Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+app.use('/api/auth', auth);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
