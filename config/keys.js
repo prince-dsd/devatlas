5b96a498dcfc2e411651550ce96dd6dca836703f
sudo-corvus <neo11prince@gmail.com>
2020-07-29 18:42:39 +0530

    user route fixes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb+srv://bitnet:bitnet@123@cluster0-rl1u3.mongodb.net/test?retryWrites=true&w=majority/devconnect`
+}
--- server.js
+++ server.js
@@ -1,46 +1,31 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const mongoose = require('mongoose');
 const path = require('path');
-
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-const auth = require('./routes/api/auth');
-
-//App init
 
 const app = express();
 
 // Connect Database
 connectDB();
 
-
-
-
-
 // Init Middleware
 app.use(express.json());
 
-
-//User Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-app.use('/api/auth', auth);
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
